68d79e5a9ada276982841f64ded5fd1aba977efb
admin <admin@admins-MacBook-Pro.local>
2025-10-20 12:06:07 +0530

    add route for the update expense function

--- routes/expenseRoutes.ts
+++ routes/expenseRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getAllExpenses,addExpense} from "../controllers/expenseController.ts";
+import { getAllExpenses,addExpense,updatedExpense} from "../controllers/expenseController.ts";
 
 const router = Router();
 
 router.get('/expenses',getAllExpenses);
 router.post('/expenses',addExpense);
-
+router.put('/expenses/:id',updatedExpense);
 
 
 
