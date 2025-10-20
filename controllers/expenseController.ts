d457efc206e3cabb437a7de61231cca3681b11c1
admin <admin@admins-MacBook-Pro.local>
2025-10-20 12:34:26 +0530

    update the addExpense to add data to the firebase

--- controllers/expenseController.ts
+++ controllers/expenseController.ts
@@ -13,4 +13,21 @@ export const getAllExpenses = async (req :Request,res:Response) =>{
     res.status(200).send(expenses);
 };
 
+export const addExpense = async (req :Request,res:Response)=>{
+    const expenseCollection = db.collection('expenses');
+    const { description, amount, category, date } = req.body;
+
+
+    const newExpense = {
+        description: description,
+        amount: parseFloat(amount),
+        category: category,
+        date: date,
+    }
+
+    const addRef = await expenseCollection.add(newExpense);
+    res.status(201).send({ id:addRef.id, ...newExpense});
+};
+
+
 
