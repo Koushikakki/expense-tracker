1b25b9d8a01cf5e4c8ed6d0d843ce4405d8fe77b
admin <admin@admins-MacBook-Pro.local>
2025-10-20 12:08:36 +0530

    add a route for the deleteExpense

--- routes/expenseRoutes.ts
+++ routes/expenseRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getAllExpenses,addExpense,updatedExpense} from "../controllers/expenseController.ts";
+import { getAllExpenses,addExpense,updatedExpense, deleteExpense} from "../controllers/expenseController.ts";
 
 const router = Router();
 
 router.get('/expenses',getAllExpenses);
 router.post('/expenses',addExpense);
 router.put('/expenses/:id',updatedExpense);
-
+router.delete('/expenses/:id',deleteExpense);
 
 
 export default router;
