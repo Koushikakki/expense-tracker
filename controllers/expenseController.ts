2e7167ffdd0723442114f849f812dcedf63157fa
admin <admin@admins-MacBook-Pro.local>
2025-10-20 11:58:13 +0530

    implement addExpense function to add a expense

--- controllers/expenseController.ts
+++ controllers/expenseController.ts
@@ -6,5 +6,21 @@ export const getAllExpenses = (req : Request, res : Response)=>{
     res.status(200).send(expenses);
 };
 
+export const addExpense = (req : Request, res: Response) => {
+    console.log("got a post request");
+
+    const { description, amount, category, date } = req.body;
+    const newExpense = {
+        id: (expenses[expenses.length-1].id)+1,
+        description: description,
+        amount: parseFloat(amount),
+        category: category,
+        date: date,
+    }
+
+    expenses.push(newExpense);
+    res.status(201).send(newExpense);
+};
+
 
 
