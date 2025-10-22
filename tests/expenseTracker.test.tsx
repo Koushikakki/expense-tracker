2bee6be5b31990133c35cebc2498eb86c1bfa3a4
admin <admin@admins-MacBook-Pro.local>
2025-10-22 12:03:50 +0530

    test for alert window when input is empty

--- tests/expenseTracker.test.tsx
+++ tests/expenseTracker.test.tsx
@@ -1,4 +1,4 @@
-import {render ,screen ,fireEvent,act} from  '@testing-library/react';
+import {render ,screen ,fireEvent,act, waitFor} from  '@testing-library/react';
 import ExpenseTracker from '../src/App.tsx';
 import '@testing-library/jest-dom';
 
@@ -38,4 +38,26 @@ test('opens model when click on add Expenses ',async ()=>{
     expect(model).toBeInTheDocument();
 
 });
+
+test('shows alert message when input field is empty',async()=>{
+    window.alert = jest.fn();
+
+    await act (async ()=>{
+        render(<ExpenseTracker/>);
+    });
+
+    
+    fireEvent.click(screen.getByText(/Add Expense/i));
+
+    const addButton = await screen.getAllByRole("button",{name :/Add Expense/i });
+    const submitButton= addButton[1];
+    fireEvent.click(submitButton);
+
+    await waitFor(()=>{
+        expect(window.alert).toHaveBeenCalledWith("Enter all the data fields");
+    });
+
+});
+
+
 
